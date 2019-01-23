0b5c843b25571878964b7217dfb9d0800a1e4244
TinQk <potie.quentin@gmail.com>
2019-01-23 16:11:08 +0100

    migrations Item Cart Order and association tables

---
+++ db/migrate/20190123142203_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :title, null: false
+      t.text :description
+      t.decimal :original_price
+      t.boolean :has_discount, default: false
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190123142313_create_item_orders.rb
@@ -0,0 +1,11 @@
+class CreateItemOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :item_orders do |t|
+      t.references :order
+      t.references :item
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
