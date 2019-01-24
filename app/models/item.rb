1113195adf25ee5df24299b4028144340fc668b8
Olivier <orohellec44@gmail.com>
2019-01-24 14:28:52 +0100

    annotate the models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: items
+#
+#  id                  :bigint(8)        not null, primary key
+#  title               :string           not null
+#  description         :text
+#  original_price      :decimal(, )
+#  has_discount        :boolean          default(FALSE)
+#  image_url           :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  discount_percentage :integer          default(0)
+#
+
 class Item < ApplicationRecord
   validates :title, presence: true
 
