4a796a1d6fd6394957bce113544f2106b5f3878e
Olivier <orohellec44@gmail.com>
2019-01-25 18:25:15 +0100

    add confirmable option to devise. maildev in development and sendgrind in production, make the seed work with confirmable option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Sendgrind
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRIND_USERNAME'],
+    :password => ENV['SENDGRIND_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
