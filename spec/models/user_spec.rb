a69e0bbf39e1f6e118af7c040f8a194a6987cec0
charles_mahaco <charles.belpois@gmail.com>
2019-01-25 13:58:07 +0100

    devise model associations + rspec fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe User, type: :model do
 
   context "with valid Factory" do
     it "has a valid factory" do
-      expect(build(:admin)).to be_valid
+      expect(build(:user)).to be_valid
     end
   end
 end
