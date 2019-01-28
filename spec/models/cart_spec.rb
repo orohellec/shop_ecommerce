ec4c2f14d0fd6da15b7bba58d832890e79e53b77
charles_mahaco <charles.belpois@gmail.com>
2019-01-28 22:35:11 +0100

    databas tests on cart/cart_item models

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -13,5 +13,14 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Model instantiation' do
+    subject(:new_cart) { described_class.new }
+
+    describe 'Database' do
+      it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
+      it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+      it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    end
+  end
 end
