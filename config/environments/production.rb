7b1e4317efc22e87f07c11258baf1e56e8fde68c
Olivier <orohellec44@gmail.com>
2019-01-28 15:43:02 +0100

    try to make sendgrind work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => "https://ecommerce-next-staging.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
   # Sendgrind
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRIND_USERNAME'],
