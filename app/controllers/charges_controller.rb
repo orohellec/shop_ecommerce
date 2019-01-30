38c1b0890cd70d903fe2514ee7c971032f8086de
TinQk <potie.quentin@gmail.com>
2019-01-30 15:19:20 +0100

    rubocop

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,23 @@
+# frozen_string_literal: true
+
 class ChargesController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     # Amount in cents
     @amount = 500
 
     customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+    _charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
     )
-
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
