b3edb43999e26036e2ff17566255d6cdc71050f7
Olivier <orohellec44@gmail.com>
2019-01-30 19:52:53 +0100

    an admin can now edit and delete an article

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -6,6 +6,45 @@ module Administration
       @items = Item.all
     end
 
-    def update; end
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_parameters)
+      @item.save
+      redirect_to administration_items_path
+    end
+
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_parameters)
+      redirect_to administration_items_path
+    end
+
+    def show
+      @item = Item.find(params[:id])
+    end
+
+    def destroy
+      @item = Item.find(params[:id])
+      @item.delete
+      redirect_to administration_items_path
+    end
+
+    private
+
+    def item_parameters
+      params.require(:item).permit(
+        :image_url,
+        :title,
+        :description,
+        :original_price
+      )
+    end
   end
 end
