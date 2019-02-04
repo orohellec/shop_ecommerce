f3a3d975e09c145b0adf9cf486d21247bb30051a
charles_mahaco <charles.belpois@gmail.com>
2019-02-04 19:15:46 +0100

    Root set to Items index, new items index front & ugly modals (for now)

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'items#index'
   get '/cart', to: 'cart#show'
   resources :items, only: %i[index show]
   resources :cart_items, only: %i[create update destroy]
