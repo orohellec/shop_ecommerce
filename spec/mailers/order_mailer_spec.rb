0c8fffbc74f1c9ed55413244ca0e2623f8634ef8
TinQk <potie.quentin@gmail.com>
2019-02-05 18:03:17 +0100

    rubocop

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -2,6 +2,6 @@
 
 require "rails_helper"
 
-RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe OrderMailer, type: :mailer do
+# pending "add some examples to (or delete) #{__FILE__}"
+# end
