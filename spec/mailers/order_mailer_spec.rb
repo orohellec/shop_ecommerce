d712e26778df5eb418cb47225d7c5a82c9ac0df8
TinQk <potie.quentin@gmail.com>
2019-02-08 16:26:13 +0100

    rubocop

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -2,6 +2,27 @@
 
 require "rails_helper"
 
-# RSpec.describe OrderMailer, type: :mailer do
-# pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe OrderMailer, type: :mailer do
+  describe "user_order" do
+    let( :user ){ create :user, email: 'test_user@example.com' }
+    let( :mail ){ OrderMailer.user_order_email(user) }
+
+    it "got queued" do
+      assert_emails 1 do
+        mail.deliver_now
+      end
+    end
+
+    it "is from the good sender" do
+      expect(mail.from).to be(["thpnextfever@gmail.com"])
+    end
+
+    it "is send to the good adress" do
+      expect(mail.to).to be(["test_user@example.com"])
+    end
+
+    it "is has the good title" do
+      expect(mail.subject).to be("Thanks for your order!")
+    end
+  end
+end
