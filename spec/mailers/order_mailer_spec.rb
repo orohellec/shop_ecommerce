4db2e93408e11ce145493ef6ba4dfe83ce25b2ca
TinQk <potie.quentin@gmail.com>
2019-02-08 16:42:39 +0100

    rubocop fix

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -14,15 +14,39 @@ RSpec.describe OrderMailer, type: :mailer do
     end
 
     it "is from the good sender" do
-      expect(mail.from).to be(["thpnextfever@gmail.com"])
+      expect(mail.from).to eq(["thpnextfever@gmail.com"])
     end
 
     it "is send to the good adress" do
-      expect(mail.to).to be(["test_user@example.com"])
+      expect(mail.to).to eq(["test_user@example.com"])
     end
 
     it "is has the good title" do
-      expect(mail.subject).to be("Thanks for your order!")
+      expect(mail.subject).to eq("Thanks for your order!")
+    end
+  end
+
+  describe "admin_order" do
+    let( :admin ){ create :admin, email: 'test_admin@example.com' }
+    let( :user ){ create :user, email: 'test_user@example.com' }
+    let( :mail ){ OrderMailer.admin_order_email(user) }
+
+    it "got queued" do
+      assert_emails 1 do
+        mail.deliver_now
+      end
+    end
+
+    it "is from the good sender" do
+      expect(mail.from).to eq(["thpnextfever@gmail.com"])
+    end
+
+    it "is send to the good adress" do
+      expect(mail.to).to eq(["thpnextfever@gmail.com"])
+    end
+
+    it "is has the good title" do
+      expect(mail.subject).to eq("An order has been placed")
     end
   end
 end
